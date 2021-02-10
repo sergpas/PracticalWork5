db4f1cf55bf598b61480b9262fb84e1b91c74604
Admin <sergpas@kil.com>
2021-02-10 21:50:46 +0200

    Work

--- src/com/alfa/work53/Main.java
+++ src/com/alfa/work53/Main.java
@@ -5,7 +5,7 @@ public class Main {
         String inString = "An information  system  is  designed  to  collect,  process, store and distribute information";
         int i = inString.indexOf(' ');
         int j = inString.lastIndexOf(' ');
-        String outString = inString.substring(j+1)+inString.substring(i,j+1)+inString.substring(0,i);
+        String outString = inString.substring(j + 1) + inString.substring(i, j + 1) + inString.substring(0, i);
         System.out.println(inString);
         System.out.println(outString);
     }
--- src/com/alfa/work55/Main.java
+++ src/com/alfa/work55/Main.java
@@ -1,4 +1,5 @@
 package com.alfa.work55;
+
 import java.util.Scanner;
 
 public class Main {
@@ -6,12 +7,12 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter String: ");
         String inString = scan.nextLine();
-        int i=0;
+        int i = 0;
         System.out.println("Unique Bytes: ");
-        while(i < inString.length()){
+        while (i < inString.length()) {
             char chr = inString.charAt(i);
-            if(inString.indexOf(chr) == inString.lastIndexOf(chr)) {
-                System.out.println(chr+" ");
+            if (inString.indexOf(chr) == inString.lastIndexOf(chr)) {
+                System.out.println(chr + " ");
             }
             i++;
         }
