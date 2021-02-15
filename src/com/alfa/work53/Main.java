a132cd7d814b3996543fdde15dbf09c4942a5e7b
Admin <sergpas@kil.com>
2021-02-15 16:16:12 +0200

    Work

--- src/com/alfa/work53/Main.java
+++ src/com/alfa/work53/Main.java
@@ -3,9 +3,12 @@ package com.alfa.work53;
 public class Main {
     public static void main(String[] args) {
         String inString = "An information  system  is  designed  to  collect,  process, store and distribute information";
-        int i = inString.indexOf(' ');
-        int j = inString.lastIndexOf(' ');
-        String outString = inString.substring(j + 1) + inString.substring(i, j + 1) + inString.substring(0, i);
+        int idxFirstWorld = inString.indexOf(' ');
+        int idxLastWorld = inString.lastIndexOf(' ');
+        String lastWorld = inString.substring(idxLastWorld + 1);
+        String middleText = inString.substring(idxFirstWorld, idxLastWorld + 1);
+        String firstWorld = inString.substring(0, idxFirstWorld);
+        String outString =  lastWorld + middleText + firstWorld;
         System.out.println(inString);
         System.out.println(outString);
     }
